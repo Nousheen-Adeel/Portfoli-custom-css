84450d04c090a7fdcd5034db2dcc2bf8fd2ffc45
Nousheen-Adeel <nousheenfatimaadeel@gmail.com>
2025-02-07 02:52:43 +0500

    update navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,24 +1,32 @@
-
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 import Image from 'next/image'
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className='Navbar'>
-        <Image src="/Images/nfa.png" alt="image" width={200} height={200}/>
-      <ul className='md:flex hidden'>
+      {/* Logo */}
+      <Image src="/Images/nfa.png" alt="logo" width={120} height={120} />
+
+      {/* Hamburger Menu (for mobile) */}
+      <button className="hamburger" onClick={() => setIsOpen(!isOpen)}>
+        ☰
+      </button>
+
+      {/* Navigation Links (Desktop + Mobile) */}
+      <ul className={`nav-links ${isOpen ? 'open' : ''}`}>
         <li><Link href="/">Home</Link></li>
         <li><Link href="/about">About</Link></li>
         <li><Link href="/projects">Projects</Link></li>
         <li><Link href="/contact">Contact</Link></li>
       </ul>
-      <div>
-        <button className='cv'>Download CV</button>
-      </div>
 
+      {/* Download CV Button */}
+      <button className='cv'>Download CV</button>
     </div>
   )
 }
 
-export default Navbar
+export default Navbar;
